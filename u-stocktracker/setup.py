b30793c4539871a1bd83c9e370543bd051898b77
iffthomas <thomasiff@outlook.de>
2024-01-21 16:10:45 +0100

    first commit

---
+++ u-stocktracker/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Placeholder text for now',
+    author='iffm, ifft',
+    license='MIT',
+)
